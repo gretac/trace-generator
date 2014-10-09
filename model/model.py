6698cc5d53d3d65322908ab9c3e41776d4b4e21b
mohamadissawi <moe.issawi@gmail.com>
2014-10-09 10:05:56 -0400

    basic statistic collection from traces (corrected...)

--- model/model.py
+++ model/model.py
@@ -2,37 +2,6 @@ import os
 import random
 import re
 
-
-### READ CONFIG FILE ###
-config = read_config()
-
-### READ TRACE ###
-info = read_trace()
-
-
-### DETERMINE LENGTH OF TRACE ###
-lake = int(len(info))
-
-### INITIALIZE TOTAL ###
-
-if config[1].split()[0] == '1':
-    total = event_class()
-
-if config[1].split()[0] == '0':
-    total = event()
-    
-
-
-### FINAL AND INITIAL TIMESTAMPS (in microseconds) ###
-final_time = int(info[lake-1].split()[0][2:], 16)*(10**(-3))
-initial_time = int(info[20].split()[0][2:], 16)*(10**(-3))
-
-### FINAL TIMESTAMP CORRECTED TO SUBTRACT INITIAL TIME ###
-correct_time = (final_time) - (initial_time)
-
-### FINAL STATEMENT ###
-print "The event of interest occured", total, "times in the trace, which translates to", total/correct_time, "times per microsecond."
-
 def event():
     total = 0
     print lake
@@ -67,3 +36,35 @@ def read_trace():
     info = f.readlines()
     f.close
     return info
+
+
+### READ CONFIG FILE ###
+config = read_config()
+
+### READ TRACE ###
+info = read_trace()
+
+
+### DETERMINE LENGTH OF TRACE ###
+lake = int(len(info))
+
+### INITIALIZE TOTAL ###
+
+if config[1].split()[0] == '1':
+    total = event_class()
+
+if config[1].split()[0] == '0':
+    total = event()
+    
+
+
+### FINAL AND INITIAL TIMESTAMPS (in microseconds) ###
+final_time = int(info[lake-1].split()[0][2:], 16)*(10**(-3))
+initial_time = int(info[20].split()[0][2:], 16)*(10**(-3))
+
+### FINAL TIMESTAMP CORRECTED TO SUBTRACT INITIAL TIME ###
+correct_time = (final_time) - (initial_time)
+
+### FINAL STATEMENT ###
+print "The event of interest occured", total, "times in the trace, which translates to", total/correct_time, "times per microsecond."
+
